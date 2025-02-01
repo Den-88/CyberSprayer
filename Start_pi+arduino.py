e20cdecabe03810af40ac6372e1a7fc2028b8737
Denis Shakurov <den_shak@mail.ru>
2025-02-02 01:45:00 +0300

    Fix arduino_rp 2

--- Start_pi+arduino.py
+++ Start_pi+arduino.py
@@ -242,7 +242,7 @@ def main():
         # Управляем светодиодом на ардуино и форсункой
         board.digital[led_pin].write(green_detected)  # Переключаем светодиод
         board.digital[relay_pin].write(not spray_active)  # Переключаем форсунку
-        print(green_detected + spray_active)
+        print(str(green_detected) + "   " + str(spray_active))
 
         # Добавление текста и статуса
         draw_text_with_background(
