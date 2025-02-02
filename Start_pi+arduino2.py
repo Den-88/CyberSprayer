377323a7c9803fc8e80fc6fb3da61d0c072fa519
Denis Shakurov <den_shak@mail.ru>
2025-02-02 04:03:10 +0300

    Fix arduino_rp 17

--- Start_pi+arduino2.py
+++ Start_pi+arduino2.py
@@ -1,9 +1,8 @@
 import threading
 import time
-
 import cv2
 import numpy as np
-from pyfirmata2 import Arduino, util
+from pyfirmata2 import Arduino
 import signal
 import sys
 
@@ -24,23 +23,9 @@ def detect_green(frame):
     green_ratio = green_pixels / total_pixels
     return green_ratio > 0.005, green_ratio  # Порог: 0.5% зелёных пикселей
 
-
-# Функция для добавления текста с фоном на изображение
-# def draw_text_with_background(frame, text, position, font, scale, color, thickness, bg_color, alpha=0.5):
-#     text_size, _ = cv2.getTextSize(text, font, scale, thickness)
-#     text_w, text_h = text_size
-#     x, y = position
-#     bg_x1, bg_y1 = x - 5, y - text_h - 5
-#     bg_x2, bg_y2 = x + text_w + 5, y + 5
-#     overlay = frame.copy()
-#     cv2.rectangle(overlay, (bg_x1, bg_y1), (bg_x2, bg_y2), bg_color, -1)
-#     cv2.addWeighted(overlay, alpha, frame, 1 - alpha, 0, frame)
-#     cv2.putText(frame, text, (x, y), font, scale, color, thickness)
-
-
 # Параллельный поток для захвата кадров
 class FrameCaptureThread(threading.Thread):
-    def __init__(self, rtsp_url):
+    def __init__(self, rtsp_url, frame_rate=30):
         threading.Thread.__init__(self)
         self.cap = cv2.VideoCapture(
             f"rtspsrc location={rtsp_url} protocols=tcp latency=10 ! rtph265depay ! h265parse ! avdec_h265 ! videoconvert ! appsink",
@@ -49,14 +34,18 @@ class FrameCaptureThread(threading.Thread):
         self.frame = None
         self.lock = threading.Lock()
         self.running = True
+        self.frame_rate = frame_rate  # Число кадров в секунду
+        self.last_capture_time = 0  # Время последнего захвата кадра
 
     def run(self):
         while self.running:
-            ret, frame = self.cap.read()
-            if ret:
-                with self.lock:
-                    self.frame = frame
-            # time.sleep(0.01)
+            current_time = time.time()
+            if current_time - self.last_capture_time >= 1.0 / self.frame_rate:
+                ret, frame = self.cap.read()
+                if ret:
+                    with self.lock:
+                        self.frame = frame
+                    self.last_capture_time = current_time
 
     def stop(self):
         self.running = False
@@ -84,9 +73,6 @@ def main():
     spray_end_time = 0
     running = True
 
-    # Устанавливаем обработчик сигнала для корректного завершения программы
-    # signal.signal(signal.SIGINT, lambda sig, frame: signal_handler(sig, frame, running))
-
     while running:
         start_time = time.time()
 
@@ -111,70 +97,11 @@ def main():
         board.digital[relay_pin].write(not spray_active)  # Переключаем форсунку
         print(f"Green ratio: {green_ratio:.6f}, Detected: {green_detected}, Spray: {spray_active}")
 
-        # # Добавление текста и статуса
-        # draw_text_with_background(
-        #     frame,
-        #     f"GREEN: {green_ratio * 100:.2f}%",
-        #     (10, 30),
-        #     cv2.FONT_HERSHEY_SIMPLEX,
-        #     1,
-        #     (255, 255, 255),
-        #     2,
-        #     (0, 0, 0),
-        # )
-        #
-        # spray_status = "Spray: ON" if spray_active else "Spray: OFF"
-        # spray_color = (0, 255, 0) if spray_active else (0, 0, 255)
-        # draw_text_with_background(
-        #     frame,
-        #     spray_status,
-        #     (10, 140),
-        #     cv2.FONT_HERSHEY_SIMPLEX,
-        #     1,
-        #     spray_color,
-        #     2,
-        #     (0, 0, 0),
-        #     alpha=0.5
-        # )
-        #
-        # # Отображение результата
-        # if green_detected:
-        #     draw_text_with_background(
-        #         frame,
-        #         "Green detected!",
-        #         (10, 70),
-        #         cv2.FONT_HERSHEY_SIMPLEX,
-        #         1,
-        #         (0, 255, 0),
-        #         2,
-        #         (0, 0, 0),
-        #     )
-        # else:
-        #     draw_text_with_background(
-        #         frame,
-        #         f"No green detected!",
-        #         (10, 70),
-        #         cv2.FONT_HERSHEY_SIMPLEX,
-        #         1,
-        #         (0, 0, 255),
-        #         2,
-        #         (0, 0, 0),
-        #     )
-        #
-        # # Показ кадра
-        # cv2.imshow("Green Color Detection", frame)
-
-    #     # Проверка нажатия клавиши 'q' для выхода
-    #     key = cv2.waitKey(1) & 0xFF
-    #     if key == ord("q"):
-    #         running = False
-    #
-    #     #
         end_time = time.time()
         print(f"Frame processed in {end_time - start_time:.4f} seconds")
-    #
-    # capture_thread.stop()
-    # cv2.destroyAllWindows()
+
+    capture_thread.stop()
+    cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
