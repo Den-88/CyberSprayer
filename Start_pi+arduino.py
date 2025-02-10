c02c227f9d9c3b48ea019b437013c4bbac945088
Denis Shakurov <den_shak@mail.ru>
2025-02-11 02:09:01 +0300

    Fix arduino_rp 75

--- Start_pi+arduino.py
+++ Start_pi+arduino.py
@@ -54,7 +54,7 @@ class FrameCaptureThread(threading.Thread):
         # )
 
         self.cap = cv2.VideoCapture(
-            f"rtspsrc location={rtsp_url} protocols=tcp latency=0 drop=true ! rtph265depay ! h265parse ! avdec_h265 ! queue max-size-buffers=1 ! videoconvert ! appsink",
+            f"rtspsrc location={rtsp_url} protocols=tcp latency=0 drop=true ! rtph265depay ! h265parse ! avdec_h265 ! videoconvert ! appsink sync=false",
             cv2.CAP_GSTREAMER
         )
         # rtsp_url = "rtsp://192.168.1.203:8555/profile0"
