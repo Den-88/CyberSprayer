887ced6d2a897afdb491ca0886d024bb041808b6
Denis Shakurov <den_shak@mail.ru>
2025-02-11 17:25:43 +0300

    Fix arduino_rp 110

--- Final.py
+++ Final.py
@@ -77,8 +77,13 @@ class FrameCaptureThread(threading.Thread):
     """Поток для захвата кадров из RTSP-потока."""
     def __init__(self, rtsp_url):
         threading.Thread.__init__(self)
+        # self.cap = cv2.VideoCapture(
+        #     f"rtspsrc location={rtsp_url} protocols=tcp latency=0 ! rtph264depay ! h264parse ! avdec_h264 ! queue max-size-buffers=1 ! videoconvert ! appsink sync=false",
+        #     cv2.CAP_GSTREAMER
+        # )
+
         self.cap = cv2.VideoCapture(
-            f"rtspsrc location={rtsp_url} protocols=tcp latency=0 ! rtph264depay ! h264parse ! openh264dec ! queue max-size-buffers=1 ! videoconvert ! appsink sync=false",
+            f"rtspsrc location={rtsp_url} protocols=tcp latency=0 ! rtph264depay ! h264parse ! h264_omx ! queue max-size-buffers=1 ! videoconvert ! appsink sync=false",
             cv2.CAP_GSTREAMER
         )
 
