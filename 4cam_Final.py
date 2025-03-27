88b7e36dedcf0a409e1ac8f3aaea06f22814b152
Denis Shakurov <den_shak@mail.ru>
2025-03-27 23:25:37 +0300

    Fix arduino_rp 163

--- 4cam_Final.py
+++ 4cam_Final.py
@@ -18,9 +18,9 @@ MIN_OBJECT_AREA = 150
 # Настройки RTSP
 RTSP_URLS = [
     "rtsp://admin:user12345@192.168.1.201:8555/main",  # Камера 1
-    "rtsp://admin:user12345@192.168.1.203:8555/main",  # Камера 2
-    "rtsp://admin:user12345@192.168.1.201:8555/main",  # Камера 3
-    "rtsp://admin:user12345@192.168.1.203:8555/main",  # Камера 4
+    "rtsp://admin:user12345@192.168.1.202:8555/main",  # Камера 2
+    "rtsp://admin:user12345@192.168.1.203:8555/main",  # Камера 3
+    "rtsp://admin:user12345@192.168.1.204:8555/main",  # Камера 4
 ]
 
 RTSP_OUTPUT_PIPELINE = (
