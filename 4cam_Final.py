d0b0c58787168dacf4e48f9f730d0ab27a354875
Denis Shakurov <den_shak@mail.ru>
2025-03-27 03:23:27 +0300

    Fix arduino_rp 151

--- 4cam_Final.py
+++ 4cam_Final.py
@@ -20,7 +20,7 @@ RTSP_URLS = [
     "rtsp://admin:user12345@192.168.1.201:8555/main",  # Камера 1
     "rtsp://admin:user12345@192.168.1.203:8555/main",  # Камера 2
     "rtsp://admin:user12345@192.168.1.201:8555/main",  # Камера 3
-    # "rtsp://admin:user12345@192.168.1.203:8555/main",  # Камера 4
+    "rtsp://admin:user12345@192.168.1.203:8555/main",  # Камера 4
 ]
 
 RTSP_OUTPUT_PIPELINE = (
