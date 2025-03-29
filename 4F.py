38f5d948d4796b29d6db5ff79b1bd368d9e1cb59
Denis Shakurov <den_shak@mail.ru>
2025-03-29 15:05:33 +0300

    Fix arduino_rp 214

--- 4F.py
+++ 4F.py
@@ -269,4 +269,4 @@ async def main():
 if __name__ == "__main__":
     # Обработчик сигнала SIGINT для корректного завершения программы
     signal.signal(signal.SIGINT, signal_handler)
-    main()
+    asyncio.run(main())
