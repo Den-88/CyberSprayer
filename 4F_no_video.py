b6791df8fe84faffbafdd634fad7c9df314c8439
Denis Shakurov <den_shak@mail.ru>
2025-03-30 15:37:08 +0300

    Fix arduino_rp 304

--- 4F_no_video.py
+++ 4F_no_video.py
@@ -158,22 +158,13 @@ def detect_green(frame):
         # Возвращаем результат и отфильтрованные контуры
         return len(filtered_contours) > 0, filtered_contours
     else:
-        # # Создаем маску
-        # mask = cv2.inRange(hsv, lower_green, upper_green)
-        # # Проверяем количество зеленых пикселей
-        # if np.count_nonzero(mask) > MIN_GREEN_PIXELS:
-        #     return True, []
-        # else:
-        #     return False, []
-        # Используем итератор, чтобы проверять пиксели по мере обработки
-        green_count = 0
-        for pixel in np.nditer(mask):
-            if pixel > 0:  # Если пиксель зелёный
-                green_count += 1
-                if green_count >= MIN_GREEN_PIXELS:
-                    return True, []  # Достигли порога, сразу возвращаем True
-        return False, []  # Если не набрали нужное количество пикселей
-
+        # Создаем маску
+        mask = cv2.inRange(hsv, lower_green, upper_green)
+        # Проверяем количество зеленых пикселей
+        if cv2.countNonZero(mask) > MIN_GREEN_PIXELS:
+            return True, []
+        else:
+            return False, []
 
 class FrameCaptureThread(threading.Thread):
     """Поток для захвата кадров из RTSP-потока."""
