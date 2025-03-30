bbd2da4a5cca31504fd01cb1c08ba68115d42ee6
Denis Shakurov <den_shak@mail.ru>
2025-03-30 16:01:34 +0300

    Fix arduino_rp 312

--- 4F_no_video.py
+++ 4F_no_video.py
@@ -160,8 +160,6 @@ def detect_green(frame):
         # Возвращаем результат и отфильтрованные контуры
         return len(filtered_contours) > 0, filtered_contours
     else:
-        # Создаем маску
-        mask = cv2.inRange(hsv, lower_green, upper_green)
         # Проверяем количество зеленых пикселей
         if np.count_nonzero(mask) > MIN_GREEN_PIXELS:
         # if cv2.countNonZero(mask) > MIN_GREEN_PIXELS:
@@ -169,16 +167,15 @@ def detect_green(frame):
         else:
             return False, []
 
-
-        # Преобразуем маску в одномерный массив и проверяем частями
-        flat_mask = mask.ravel()
-        # Используем быструю итерацию с ранним выходом
-        green_count = 0
-        for i in range(0, len(flat_mask), 10000):  # Читаем по 1000 пикселей за раз
-            green_count += np.count_nonzero(flat_mask[i:i + 10000])
-            if green_count >= MIN_GREEN_PIXELS:
-                return True, []  # Достигли порога, выходим сразу
-        return False, []  # Недостаточно зелёного
+        # # Преобразуем маску в одномерный массив и проверяем частями
+        # flat_mask = mask.ravel()
+        # # Используем быструю итерацию с ранним выходом
+        # green_count = 0
+        # for i in range(0, len(flat_mask), 10000):  # Читаем по 1000 пикселей за раз
+        #     green_count += np.count_nonzero(flat_mask[i:i + 10000])
+        #     if green_count >= MIN_GREEN_PIXELS:
+        #         return True, []  # Достигли порога, выходим сразу
+        # return False, []  # Недостаточно зелёного
 
 
 class FrameCaptureThread(threading.Thread):
